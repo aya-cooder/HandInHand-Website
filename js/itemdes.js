a4bf03fb36469050df41da778d259ad45e7494dc
aya-cooder <52712767+aya-cooder@users.noreply.github.com>
2020-06-13 02:00:31 +0200

    final handmade edit ,item edit

--- js/itemdes.js
+++ js/itemdes.js
@@ -23,3 +23,17 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
+/*** select photo */
+function readURL(input) {
+  if (input.files && input.files[0]) {
+      var reader = new FileReader();
+
+      reader.onload = function (e) {
+          $('#blah')
+              .attr('src', e.target.result);
+      };
+
+      reader.readAsDataURL(input.files[0]);
+  }
+}
