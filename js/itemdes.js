028b9f7104ae5f4fde646168994bcbe14c318267
aya-cooder <52712767+aya-cooder@users.noreply.github.com>
2020-06-15 18:00:37 +0200

    testing handmade description

--- js/itemdes.js
+++ js/itemdes.js
@@ -33,7 +33,32 @@ function readURL(input) {
           $('#blah')
               .attr('src', e.target.result);
       };
-
       reader.readAsDataURL(input.files[0]);
+  }
+}
+/*************************/
+// Get the modal
+var modalete = document.getElementById("myModalete");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtnreport");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("closereport")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modalete.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modalete.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modalete) {
+    modalete.style.display = "none";
   }
 }
